db45f2fab5fbd2c8ead2d87cf2fd458fee749812
XiaoqianZHang <zxqaiycx@qq.com>
2019-07-02 12:32:50 +0800

    add solution to #19 (in need of improvment)

---
+++ zxq/0019_remove_nth_node_from_end_of_list.cpp
@@ -0,0 +1,47 @@
+/**
+ * 问题描述
+ * 19. Remove Nth Node From End of List
+ * Given a linked list, remove the n-th node from the end of list and return its head.
+ * Note: Given n will always be valid.
+ * Follow up: Could you do this in one pass?
+ */
+
+/**
+ * 提交结果
+ * Runtime: 4 ms, faster than 92.36% of C++ online submissions for Remove Nth Node From End of List. 
+ * Memory Usage: 8.6 MB, less than 24.03% of C++ online submissions for Remove Nth Node From End of List.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int len = 0;
+        ListNode* nextNode = head;
+        
+        while (nextNode != NULL) {
+            nextNode = nextNode->next;
+            ++len;
+        }
+        
+        if (n == len) {
+            head = head->next;
+            return head;
+        }
+        else {
+            nextNode = head;
+            for (int i = 0; i < len - n - 1; ++i) {
+                nextNode = nextNode->next;
+            }
+            nextNode->next = nextNode->next->next;
+            return head;
+        }
+    }
+};
