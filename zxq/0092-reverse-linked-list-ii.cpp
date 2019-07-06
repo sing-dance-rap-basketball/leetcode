6f21760fc9d34d881dc55e4ef68db342317c0a59
XiaoqianZhang <zxqaiycx@qq.com>
2019-07-06 18:39:19 +0800

    update #92

--- zxq/0092-reverse-linked-list-ii.cpp
+++ zxq/0092-reverse-linked-list-ii.cpp
@@ -51,3 +51,44 @@ public:
         }
     }
 };
+
+/**
+ * https://leetcode.com/fudonglai
+ * fudonglai's solution. beautiful!
+ */
+
+To solve the problem recursively, step by step.
+First, I know the classic recursive way to reverse a linked list:
+
+    ListNode reverse(ListNode head) {
+        if (head.next == null) return head;
+        ListNode last = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+
+Then I come up this way to reverse the first N elements:
+
+    ListNode successor = null;
+    ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            successor = head.next;
+            return head;
+        }
+        ListNode last = reverseN(head.next, n - 1);
+        head.next.next = head;
+        head.next = successor;
+        return last;
+    }
+
+Finally I solve this problem:
+
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (m <= 1) {
+            // You can also expand the code here to get rid of the helper function 'reverseN'
+            return reverseN(head, n - m + 1);
+        }
+        head.next = reverseBetween(head.next, m - 1, n - 1);
+        return head;
+    }
