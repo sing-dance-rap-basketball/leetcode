9306319bef36a3741b7838d2c162c8719bb66542
XiaoqianZhang <zxqaiycx@qq.com>
2019-07-12 17:37:04 +0800

    update #148

--- zxq/0148-sort-list.cpp
+++ zxq/0148-sort-list.cpp
@@ -68,13 +68,66 @@
 //     }
 // };
 
+// /*
+//   √ 16/16 cases passed (56 ms)
+//   √ Your runtime beats 45.2 % of cpp submissions
+//   √ Your memory usage beats 23.83 % of cpp submissions (24.8 MB)
+// */
+// class Solution {
+// public:
+//     ListNode* merge(ListNode* l1, ListNode* l2) 
+//     {        
+//         ListNode* dummy = new ListNode(-1);
+//         ListNode* l = dummy;
+        
+//         while (l1 && l2) {
+//             if (l1->val <= l2->val) {
+//                 l->next = l1;
+//                 l1 = l1->next;
+//             }
+//             else {
+//                 l->next = l2;
+//                 l2 = l2->next;
+//             }
+//             l = l->next;
+//         }
+//         l->next = (l1 == nullptr) ? l2 : l1;
+        
+//         return dummy->next;
+//     }
+
+//     ListNode* sortList(ListNode* head) {
+//         if (head == nullptr || head->next == nullptr) {
+//             return head;
+//         }
+//         ListNode* slow = head;
+//         ListNode* fast = head->next;
+//         while (fast != nullptr && fast->next != nullptr) {
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+//         fast = slow->next;
+//         slow->next = nullptr;
+//         return merge(sortList(head), sortList(fast));
+//     }
+// };
+
+/*
+  √ 16/16 cases passed (40 ms)
+  √ Your runtime beats 97.95 % of cpp submissions
+  √ Your memory usage beats 60.92 % of cpp submissions (11.9 MB)
+  √ 16/16 cases passed (40 ms)
+  √ Your runtime beats 97.95 % of cpp submissions
+  √ Your memory usage beats 83.72 % of cpp submissions (11.6 MB)
+*/
 class Solution {
 public:
+    ListNode* dummy;
+    ListNode* l;
+
     ListNode* merge(ListNode* l1, ListNode* l2) 
-    {        
-        ListNode* dummy = new ListNode(-1);
-        ListNode* l = dummy;
-        
+    {
+        l = dummy;
         while (l1 && l2) {
             if (l1->val <= l2->val) {
                 l->next = l1;
@@ -87,22 +140,27 @@ public:
             l = l->next;
         }
         l->next = (l1 == nullptr) ? l2 : l1;
-        
         return dummy->next;
     }
 
-    ListNode* sortList(ListNode* head) {
+    ListNode* mergeSort(ListNode* head)
+    {
         if (head == nullptr || head->next == nullptr) {
             return head;
         }
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while (fast != nullptr && fast->next != nullptr) {
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+        while (fast && fast->next) {
             slow = slow->next;
             fast = fast->next->next;
         }
         fast = slow->next;
         slow->next = nullptr;
-        return merge(sortList(head), sortList(fast));
+        return merge(mergeSort(head), mergeSort(fast));
+    }
+
+    ListNode* sortList(ListNode* head) {
+        dummy = new ListNode(-1);
+        return mergeSort(head);
     }
 };
