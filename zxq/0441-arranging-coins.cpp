bae880b652943bf3fe49a14e142c638ce788cc0e
XiaoqianZHang <zxqaiycx@qq.com>
2020-04-12 23:20:28 +0800

    Update #441

--- zxq/0441-arranging-coins.cpp
+++ zxq/0441-arranging-coins.cpp
@@ -5,6 +5,13 @@
  */
 
 // @lc code=start
+
+/**
+ * Accepted
+    1336/1336 cases passed (4 ms)
+    Your runtime beats 81.26 % of cpp submissions
+    Your memory usage beats 100 % of cpp submissions (5.9 MB)
+ */
 class Solution {
 public:
     int arrangeCoins(int n) {
