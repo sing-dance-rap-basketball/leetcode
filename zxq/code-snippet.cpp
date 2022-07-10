a16a2e2c217e26ea2a48db799ae6bece57a61b54
zxq <zxqaiycx@qq.com>
2022-07-11 02:50:04 +0800

    add code-snippet.cpp

---
+++ zxq/code-snippet.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) begin(), end(x)
+#define sz(x) (x).size()
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
